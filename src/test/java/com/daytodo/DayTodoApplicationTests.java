ac4d03fba7ccf38d3561099438be1f8dc37c2b75
yxpjseo <yjins2004@gmail.com>
2026-07-11 18:32:10 +0900

    chore: 초기환경설정

---
+++ src/test/java/com/daytodo/DayTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daytodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DayTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
