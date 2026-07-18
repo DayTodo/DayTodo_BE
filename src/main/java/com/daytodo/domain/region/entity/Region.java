bf6ad67e042f95525d798553b657a6ec5364f084
yxpjseo <yjins2004@gmail.com>
2026-07-18 17:53:29 +0900

    feat: region entity, enums 구현

---
+++ src/main/java/com/daytodo/domain/region/entity/Region.java
@@ -0,0 +1,28 @@
+package com.daytodo.domain.region.entity;
+
+import com.daytodo.domain.region.enums.RegionLevel;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "region")
+@Getter
+@NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
+public class Region {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column (name = "region_id")
+    private Long regionId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id")
+    private Region parent;
+
+    @Column (name = "region_name", nullable = false, length = 50)
+    private String regionName;
+
+    @Enumerated(EnumType.STRING)
+    @Column (name = "region_level", nullable = false, length = 20)
+    private RegionLevel regionLevel;
+}
---
+++ src/main/java/com/daytodo/domain/region/enums/RegionLevel.java
@@ -0,0 +1,6 @@
+package com.daytodo.domain.region.enums;
+
+public enum RegionLevel {
+    SIDO,      // 시/도
+    SIGUNGU    // 시/군/구
+}
