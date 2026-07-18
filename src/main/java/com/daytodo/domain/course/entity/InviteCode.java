9eec0c7980aff94c4847634497bfe89ca65efe7f
Kim Ji Hyeon <117696182+Hyeoniss@users.noreply.github.com>
2026-07-18 19:19:41 +0900

    fix: BaseEntity 상속으로 코드 수정, ParticipantType enum 수정

--- src/main/java/com/daytodo/domain/course/entity/InviteCode.java
+++ src/main/java/com/daytodo/domain/course/entity/InviteCode.java
@@ -1,11 +1,10 @@
 package com.daytodo.domain.course.entity;
 
+import com.daytodo.domain.common.BaseCreatedEntity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
@@ -13,9 +12,8 @@ import java.time.LocalDateTime;
 @Setter
 @NoArgsConstructor
 @Entity
-@EntityListeners(AuditingEntityListener.class)
 @Table(name = "InviteCode")
-public class InviteCode {
+public class InviteCode extends BaseCreatedEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -36,8 +34,4 @@ public class InviteCode {
 
     @Column(name = "is_active", nullable = false)
     private Boolean isActive;
-
-    @CreatedDate
-    @Column(name = "created_at", nullable = false, updatable = false)
-    private LocalDateTime createdAt;
 }
