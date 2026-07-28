81aa3b8cd6caedbf88c7b69c2dbe9b43f998a3a7
Kim Ji Hyeon <117696182+Hyeoniss@users.noreply.github.com>
2026-07-28 16:37:56 +0900

    fix: JwtProperties파일에 추가했던 어노테이션 재삭제

--- src/main/java/com/daytodo/global/security/JwtProperties.java
+++ src/main/java/com/daytodo/global/security/JwtProperties.java
@@ -1,9 +1,7 @@
 package com.daytodo.global.security;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
 
-@Component
 @ConfigurationProperties(prefix = "jwt.token")
 public record JwtProperties(String secretKey, Expiration expiration) {
 
