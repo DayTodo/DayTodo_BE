1ce073a7c2f2ac07ca306c34c9a449dc911b2688
yxpjseo <yjins2004@gmail.com>
2026-08-03 11:56:39 +0900

    feat: add configuration for Tour API integration and update RestClient configuration

--- src/main/java/com/daytodo/global/config/RestClientConfig.java
+++ src/main/java/com/daytodo/global/config/RestClientConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
 
 @Configuration
-@EnableConfigurationProperties(NaverApiProperties.class)
+@EnableConfigurationProperties({NaverApiProperties.class, TourApiProperties.class})
 public class RestClientConfig {
 
     @Bean
@@ -15,6 +15,14 @@ public class RestClientConfig {
                 .baseUrl(properties.baseUrl())
                 .defaultHeader("X-Naver-Client-Id", properties.clientId())
                 .defaultHeader("X-Naver-Client-Secret", properties.clientSecret())
+                .build();
+    }
+
+    // 한국관광공사 KorService2. serviceKey 는 각 호출에서 쿼리파라미터로 붙인다.
+    @Bean
+    public RestClient tourRestClient(TourApiProperties properties) {
+        return RestClient.builder()
+                .baseUrl(properties.baseUrl())
                 .build();
     }
 }
