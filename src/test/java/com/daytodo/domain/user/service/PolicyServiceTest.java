a307dba098c1951ef14eea02fed098f64b5e3745
kwonwnsduf <junyeol1009@gmail.com>
2026-08-08 22:48:30 +0900

    feat: 이용약관 문서 추가

--- src/test/java/com/daytodo/domain/user/service/PolicyServiceTest.java
+++ src/test/java/com/daytodo/domain/user/service/PolicyServiceTest.java
@@ -11,7 +11,14 @@ class PolicyServiceTest {
     void readsBothPolicyDocumentsFromClasspath() {
         UserResponse.Policies policies = new PolicyService().getPolicies();
 
-        assertThat(policies.termsOfService()).contains("DayTodo 이용약관");
-        assertThat(policies.privacyPolicy()).contains("DayTodo 개인정보처리방침");
+        assertThat(policies.termsOfService())
+                .contains("데이투두(DayToDo) 이용약관")
+                .contains("제20조 (준거법 및 관할법원)")
+                .contains("3. 위치기반서비스 이용약관");
+        assertThat(policies.privacyPolicy())
+                .contains("데이투두(DayToDo) 개인정보처리방침")
+                .contains("15) 개인정보 처리방침의 변경에 관한 사항")
+                .contains("4. 마케팅/이벤트 정보 수신 동의 안내")
+                .contains("6. 네이버 소셜 로그인 이용 시 안내");
     }
 }
